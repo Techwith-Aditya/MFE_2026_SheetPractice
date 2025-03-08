16a44baf0657291e6cc6ee51682465ba36b893f9
Techwith-Aditya <160505538+Techwith-Aditya@users.noreply.github.com>
2025-03-08 15:53:00 +0530

    SearchInSortedRotatedArray.java

---
+++ 7March, 2025/SearchInSortedRotatedArray.java
@@ -0,0 +1,59 @@
+public class SearchInSortedRotatedArray {
+    public static int search(int[] nums, int target) {
+        int left=0, right=nums.length-1;
+
+        while(left<=right) 
+        {
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target) 
+            {
+                return mid;
+            }
+
+            if(nums[left]<=nums[mid]) 
+            {
+                if(nums[left]<=target && target<nums[mid]) 
+                {
+                    right=mid-1;
+                } 
+                else 
+                {
+                    left=mid+1;
+                }
+            } 
+            else 
+            {
+                if(nums[mid]<target && target<=nums[right]) 
+                {
+                    left=mid+1;
+                } 
+                else 
+                {
+                    right=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] given={4,5,6,7,0,1,2};
+        int target=2;
+        int ans=search(given, target);
+        System.out.println(ans);
+    }
+}
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int i=0;
+//         for(i=0; i<nums.length; i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
